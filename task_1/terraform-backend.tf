e5de531e7ac39e07c8f23d3f23fcfc9183a6c75e
Eduardo Rascon <137971767+ed-cloudgithub@users.noreply.github.com>
2025-06-14 15:42:09 -0700

    Update terraform-backend.tf
    Added required providers and additional settings for S3 bucket and DynamoDB table

--- task_1/terraform-backend.tf
+++ task_1/terraform-backend.tf
@@ -4,5 +4,65 @@ terraform {
     key    = "terraform.tfstate"
     region = "us-west-1"
     dynamodb_table = "terraform-statefile"
+    encrypt = true
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.56"  
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_s3_bucket" "terraform_state_bucket" {
+  bucket        = "rsschool-awsdevops-tfstate"
+  force_destroy = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
+  bucket                  = aws_s3_bucket.terraform_state_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+  bucket                  = aws_s3_bucket.terraform_state_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
+resource "aws_dynamodb_table" "terraform-statefile" {
+  name         = "terraform-statefile"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
